723b0a0107b3079da6bac15b3caa7cf660339581
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-11 11:21:58 +0530

    Time: 3 ms (49.64%), Space: 7.5 MB (84.88%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,22 +1,22 @@
-class Solution{
+class Solution {
 public:
-        static string reverseParentheses(string & s){
-            string st, rev;
-            for (char c: s){
-                if (c != ')')
-                    st.push_back(c);
-                else{
-                    rev = "";
-                    char r = st.back();
-                    while (r != '('){
-                        rev += r;
-                        st.pop_back();
-                        r = st.back();
-                    }
+    static string reverseParentheses(string& s) {
+        string st, rev;
+        for (char c : s) {
+            if (c != ')')
+                st.push_back(c);
+            else {
+                rev = "";
+                char r = st.back();
+                while (r != '(') {
+                    rev += r;
                     st.pop_back();
-                    st += rev;
+                    r = st.back();
                 }
+                st.pop_back();
+                st += rev;
             }
-            return st;
         }
+        return st;
+    }
 };
