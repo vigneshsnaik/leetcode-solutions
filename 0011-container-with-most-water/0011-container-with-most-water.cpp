3f2c2e230f27ccff48c923f9dffa67af2ab58cfc
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-17 22:56:39 +0530

    Time: 65 ms (37.64%), Space: 61.3 MB (92.47%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,16 +1,14 @@
-// https://leetcode.com/problems/container-with-most-water
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
         auto l=height.begin();
         auto r=height.end()-1;
-        int area=0,maxArea=0;
+        int maxArea=0;
         while(l<r){
-            area=(*l<*r?*l:*r)*(r-l);
+            int area=(*l<*r?*l:*r)*(r-l);
             maxArea=(area>maxArea?area:maxArea);
-            if(*l<*r) l++;
-            else r--;
+            if(*l<*r) {l++;}
+            else {r--;}
         }
         return maxArea;
     }
