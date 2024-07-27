d3c3b228b42cf9c324e88b13cbc056279a2250fd
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-27 12:54:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<int>> d(26, vector<int>(26, INT_MAX));
+        int N = source.size();
+        for (int i = 0; i < cost.size(); i++) {
+            d[original[i] - 'a'][changed[i] - 'a'] = cost[i];
+        }
+        for (int i = 0; i < 26; ++i) {
+            d[i][i] = 0;
+        }
+        for (int k = 0; k < 26; ++k) {
+            for (int i = 0; i < 26; ++i) {
+                for (int j = 0; j < 26; ++j) {
+                    if (d[i][k] != INT_MAX && d[k][j] != INT_MAX) {
+                        d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+                    }
+                }
+            }
+        }
+        long long res = 0;
+        for (int i = 0; i < N; i++) {
+            if (source[i] != target[i]) {
+                if (d[source[i] - 'a'][target[i] - 'a'] == INT_MAX) {
+                    return -1;
+                }
+                res += d[source[i] - 'a'][target[i] - 'a'];
+            }
+        }
+        return res;
+    }
+};
