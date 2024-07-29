47e30079c7b02c2d2d060ab7677a49e30f2a5188
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-29 15:10:03 +0530

    Time: 31 ms (67.12%), Space: 11.5 MB (59.43%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
+        int n = rating.size();
         int count = 0;
-        for (auto i = rating.begin(); i != rating.end(); i++) {
-            for (auto j = i; j != rating.end(); j++) {
-                for (auto k = j; k != rating.end(); k++) {
-                    if ((*i < *j and *j < *k) or (*i > *j and *j > *k)) {
-                        count++;
-                    }
-                }
+        for (int j = 0; j < n; j++) {
+            int lessLeft = 0, greaterLeft = 0;
+            int lessRight = 0, greaterRight = 0;
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j])
+                    lessLeft++;
+                else if (rating[i] > rating[j])
+                    greaterLeft++;
             }
+            for (int k = j + 1; k < n; k++) {
+                if (rating[k] < rating[j])
+                    lessRight++;
+                else if (rating[k] > rating[j])
+                    greaterRight++;
+            }
+            count += lessLeft * greaterRight + greaterLeft * lessRight;
         }
         return count;
     }
