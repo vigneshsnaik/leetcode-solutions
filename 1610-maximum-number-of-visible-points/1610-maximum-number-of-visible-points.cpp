fbec5672d69b433ad05739f32e80cece4b2815c2
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-09-01 20:58:37 +0530

    Time: 334 ms (71.17%), Space: 167.3 MB (70.57%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.cpp
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    double diff(double a1, double a2) { return a1 - a2; }
-
-    int visiblePoints(vector<vector<int>>& points, int angle,
-                      vector<int>& location) {
+    int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& location) {
         vector<double> angles;
         int originPoints = 0;
-        for (auto& v : points) {
-            if (v == location) {
+        for (auto& point : points) {
+            if (point == location) {
                 originPoints++;
                 continue;
             }
             double angleDeg =
-                atan2(v[1] - location[1], v[0] - location[0]) * (180.0 / M_PI);
+                atan2(point[1] - location[1], point[0] - location[0]) *
+                (180.0 / M_PI);
             if (angleDeg < 0)
                 angleDeg += 360;
             angles.push_back(angleDeg);
         }
         sort(angles.begin(), angles.end());
-        if (angles.empty())
-            return originPoints;
-        int n = points.size();
+        int n = angles.size();
+        for (int i = 0; i < n; i++) {
+            angles.push_back(angles[i] + 360);
+        }
         int maxPoints = 0;
-        int i = 0, j = 0;
-        while (i < n) {
-            while (j < n + i) {
-                angles.push_back(angles[j] + 360);
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j < angles.size() && angles[j] - angles[i] <= angle) {
                 j++;
             }
-            int count = 0;
-            int start = i, end = i + n;
-            for (int k = start; k < end; k++) {
-                if (angles[k] - angles[start] <= angle) {
-                    count++;
-                }
-            }
-            maxPoints = max(maxPoints, count);
-            i++;
+            maxPoints = max(maxPoints, j - i);
         }
         return maxPoints + originPoints;
     }
