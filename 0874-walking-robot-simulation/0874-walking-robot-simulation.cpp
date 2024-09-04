47ef37ac967f1dbe4f0287b20f7ab6b0a4be3ad6
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-09-04 22:07:49 +0530

    Time: 127 ms (20.44%), Space: 42 MB (38.67%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        unordered_set<string> obs;
+        for (auto obstacle : obstacles) {
+            obs.insert(to_string(obstacle[0])+" "+to_string(obstacle[1]));
+        }
+        int x = 0, y = 0;
+        int maxDistance = 0;
+        int dx = 0, dy = 1;
+        for (int c : commands) {
+            if (c == -1) {
+                int temp = dx;
+                dx = dy;
+                dy = -temp;
+            } else if (c == -2) {
+                int temp = dx;
+                dx = -dy;
+                dy = temp;
+            } else {
+                for (int i = 0; i < c; ++i) {
+                    int xx = x + dx;
+                    int yy = y + dy;
+                    if (obs.find(to_string(xx)+" "+to_string(yy)) != obs.end())
+                        break;
+                    x = xx;
+                    y = yy;
+                    maxDistance = max(maxDistance, x * x + y * y);
+                }
+            }
+        }
+        return maxDistance;
+    }
+};
