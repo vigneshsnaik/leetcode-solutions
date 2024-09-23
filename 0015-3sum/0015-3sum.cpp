c95e7f8a109fdd468d2d57c741f5f8dfd860f560
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-09-23 21:54:39 +0530

    Time: 1259 ms (6.83%), Space: 293.9 MB (13.2%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int target = 0;
         sort(nums.begin(), nums.end());
         set<vector<int>> s;
         vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            for(int j = i+1; j < nums.size(); j++){
-                for(int k = j+1; k < nums.size(); k++){
-                    vector<int> temp;
-                    if(nums[i] + nums[j] + nums[k] == 0){
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[j]);
-                        temp.push_back(nums[k]);
-                        s.insert(temp);
-                    }
+        for (int i = 0; i < nums.size(); i++) {
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-        for(auto allTriplets : s)
+        for (auto allTriplets : s)
             output.push_back(allTriplets);
         return output;
     }
