459843119a31e17d4768499e0c68f3128e34973f
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-10-13 23:11:45 +0530

    Time: 37 ms (87%), Space: 19.4 MB (76.37%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,41 @@
+class Tracker {
+public:
+    int val, index, column;
+    Tracker(int v, int i, int c) : val(v), index(i), column(c) {}
+};
+struct CompareTracker {
+    bool operator()(const Tracker& a, const Tracker& b) {
+        return a.val > b.val;
+    }
+};
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        priority_queue<Tracker, vector<Tracker>, CompareTracker> track;
+        int maxVal = INT_MIN;
+        int n = nums.size();
+        int startRange = 0;
+        int endRange = INT_MAX;
+        for (int i = 0; i < n; ++i) {
+            int val = nums[i][0];
+            track.push(Tracker(val, i, 0));
+            maxVal = max(maxVal, val);
+        }
+        while (track.size() == n) {
+            Tracker temp = track.top();
+            track.pop();
+            if (maxVal - temp.val < endRange - startRange) {
+                startRange = temp.val;
+                endRange = maxVal;
+            }
+
+            if (temp.column + 1 < nums[temp.index].size()) {
+                int nextVal = nums[temp.index][temp.column + 1];
+                track.push(Tracker(nextVal, temp.index, temp.column + 1));
+                maxVal = max(maxVal, nextVal);
+            }
+        }
+
+        return {startRange, endRange};
+    }
+};
