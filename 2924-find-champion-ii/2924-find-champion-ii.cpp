f1a2836c5cc398387bd159b8201c40b71d5cb663
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-11-26 05:50:48 +0530

    Time: 41 ms (43.46%), Space: 104 MB (44.16%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -5,9 +5,15 @@ public:
         for (auto edge : edges) {
             inward[edge[1]] = true;
         }
-        return count(inward.begin(), inward.end(), false) == 1
-                   ? distance(inward.begin(),
-                               find(inward.begin(), inward.end(), false))
-            : -1;
+        int champion = -1;
+        for (int i = 0; i < n; i++) {
+            if (!inward[i]) {
+                if (champion != -1) {
+                    return -1;
+                }
+                champion = i;
+            }
+        }
+        return champion;
     }
 };
